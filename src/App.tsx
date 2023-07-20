5d54e610e230d8cda65246f1b6f56dbe94c26a5d
Federico Lencina <federiconahuellencina@gmail.com>
2023-07-19 22:35:12 -0300

    feat: 🎸 add navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -2,60 +2,62 @@ import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { collection, addDoc } from "firebase/firestore"; 
+import { collection, addDoc } from "firebase/firestore";
 import { db } from './firebase';
-import { getDatabase, ref, set,child, get } from "firebase/database";
+import { getDatabase, ref, set, child, get } from "firebase/database";
+import { Navbar } from './components/navBar/Navbar';
 function App() {
   const [count, setCount] = useState(0)
 
-const readData = (userId:string) => {
+  const readData = (userId: string) => {
     console.log("ADAS")
 
-const dbRef = ref(getDatabase());
-  get(child(dbRef, `users/${userId}`)).then((snapshot) => {
-  if (snapshot.exists()) {
-    console.log(snapshot.val());
-  } else {
-    console.log("No data available");
+    const dbRef = ref(getDatabase());
+    get(child(dbRef, `users/${userId}`)).then((snapshot) => {
+      if (snapshot.exists()) {
+        console.log(snapshot.val());
+      } else {
+        console.log("No data available");
+      }
+    }).catch((error) => {
+      console.error(error);
+    });
+
+    useEffect(() => {
+
+      readData("1")
+
+    }, [])
+
   }
-}).catch((error) => {
-  console.error(error);
-});
 
-useEffect(()=>{
-  
-readData("1")
 
-},[])
-  
-}
-  
+  function writeUserData(userId: string, name: any, email: any, imageUrl: any) {
+    const db = getDatabase();
+    set(ref(db, 'users/' + userId), {
+      username: name,
+      email: email,
+      profile_picture: imageUrl
+    });
+  }
 
-function writeUserData(userId: string, name: any, email: any, imageUrl: any) {
-  const db = getDatabase();
-  set(ref(db, 'users/' + userId), {
-    username: name,
-    email: email,
-    profile_picture : imageUrl
-  });
-}
-
-const test = async () =>{
-try {
-  const docRef = await addDoc(collection(db, "users"), {
-    first: "Ada",
-    last: "Lovelace",
-    born: 1815
-  });
-  console.log("Document written with ID: ", docRef.id);
-} catch (e) {
-  console.error("Error adding document: ", e);
-}
-}
+  const test = async () => {
+    try {
+      const docRef = await addDoc(collection(db, "users"), {
+        first: "Ada",
+        last: "Lovelace",
+        born: 1815
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  }
 
   return (
     <>
       <div>
+        <Navbar />
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -65,10 +67,10 @@ try {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => writeUserData("1","Fede","sddsds","asassa")}>
+        <button onClick={() => writeUserData("1", "Fede", "sddsds", "asassa")}>
           createUser
         </button>
-        <button onClick={() => writeUserData("1","Fede","sddsds","asassa")}>
+        <button onClick={() => writeUserData("1", "Fede", "sddsds", "asassa")}>
           readUser
         </button>
         <p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    './pages/**/*.{html,js,ts,tsx}',
+    './components/**/*.{html,js,ts,tsx}',
+     './public/index.html',
+    './src/**/*.{html,js,ts,tsx}',
+  ],
   theme: {
     extend: {},
   },
