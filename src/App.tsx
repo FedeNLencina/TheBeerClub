70abed5de3ac6ac9abb4fcc2db21c184d63d2956
Rodriip95 <rodrigo.m.penela@gmail.com>
2023-07-19 21:51:05 -0300

    REALG4life

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,60 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { collection, addDoc } from "firebase/firestore"; 
-import { db } from './firebase';
-import { getDatabase, ref, set,child, get } from "firebase/database";
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "./firebase";
+import { getDatabase, ref, set, child, get } from "firebase/database";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [load, setload] = useState(false)
 
-const readData = (userId:string) => {
-    console.log("ADAS")
+  const readData = (userId: string) => {
+    console.log("ADAS");
 
-const dbRef = ref(getDatabase());
-  get(child(dbRef, `users/${userId}`)).then((snapshot) => {
-  if (snapshot.exists()) {
-    console.log(snapshot.val());
-  } else {
-    console.log("No data available");
+    const dbRef = ref(getDatabase());
+    get(child(dbRef, `users/${userId}`))
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          console.log(snapshot.val());
+          setCount(snapshot.val())
+        } else {
+          console.log("No data available");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+  };
+
+  useEffect(() => {
+    readData("1");
+  }, [load]);
+
+  function writeUserData(userId: string, name: any, email: any, imageUrl: any) {
+    setload(true)
+    const db = getDatabase();
+    set(ref(db, "users/" + userId), {
+      username: name,
+      email: email,
+      profile_picture: imageUrl,
+    });
+    setload(false)
   }
-}).catch((error) => {
-  console.error(error);
-});
 
-useEffect(()=>{
-  
-readData("1")
-
-},[])
-  
-}
-  
-
-function writeUserData(userId: string, name: any, email: any, imageUrl: any) {
-  const db = getDatabase();
-  set(ref(db, 'users/' + userId), {
-    username: name,
-    email: email,
-    profile_picture : imageUrl
-  });
-}
-
-const test = async () =>{
-try {
-  const docRef = await addDoc(collection(db, "users"), {
-    first: "Ada",
-    last: "Lovelace",
-    born: 1815
-  });
-  console.log("Document written with ID: ", docRef.id);
-} catch (e) {
-  console.error("Error adding document: ", e);
-}
-}
+  const test = async () => {
+    try {
+      const docRef = await addDoc(collection(db, "users"), {
+        first: "Ada",
+        last: "Lovelace",
+        born: 1815,
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  };
 
   return (
     <>
@@ -63,13 +66,14 @@ try {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      {load && 
+      <h1>cargando</h1>
+      }
+
+      <h1>{count.username}</h1>
       <div className="card">
-        <button onClick={() => writeUserData("1","Fede","sddsds","asassa")}>
+        <button onClick={() => writeUserData("1", "asaaaad", "sddsds", "asassa")}>
           createUser
-        </button>
-        <button onClick={() => writeUserData("1","Fede","sddsds","asassa")}>
-          readUser
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
@@ -79,7 +83,7 @@ try {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
