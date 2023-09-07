08f2632406a312d1c1da59a07cd459d9caac3573
Federico Lencina <federiconahuellencina@gmail.com>
2023-09-07 20:09:28 -0300

    feat: 🎸 add context folder

--- src/components/table/TableComponent.tsx
+++ src/components/table/TableComponent.tsx
@@ -4,6 +4,7 @@ import { Table } from "../../utils/interfaces/tables/tables";
 import greenCircle from "../../assets/icons/greencircle.svg";
 import redCircle from "../../assets/icons/redcircle.svg";
 import { Dialog, Transition } from "@headlessui/react";
+import { getDatabase, ref, onValue, remove } from "firebase/database";
 
 interface TableProps {
   table?: Table;
@@ -20,6 +21,19 @@ export const TableComponent = ({ table, iconUrl }: TableProps) => {
 
   const closeModal = () => {
     setModalOpen(false);
+  };
+
+  const removeTables = () => {
+    const db = getDatabase();
+    // referencio a la lista en la base de datos
+    const postListRef = ref(db, "tablesMock");
+    remove(postListRef)
+      .then(() => {
+        console.log("Lista de tablas eliminada correctamente");
+      })
+      .catch((error) => {
+        console.error("Error al eliminar la lista de tablas:", error);
+      });
   };
 
   return (
@@ -43,42 +57,6 @@ export const TableComponent = ({ table, iconUrl }: TableProps) => {
             />
           )}
 
-          {/* {modalOpen && (
-            <div className="flex items-center justify-center fixed left-0 bottom-0 w-full h-full bg-gray-800">
-              <div className="bg-white rounded-lg w-1/2">
-                <div className="flex flex-col items-start p-4">
-                  <div className="flex items-center w-full">
-                    <div className="text-gray-900 font-medium text-lg">
-                      My modal title
-                    </div>
-                    <svg
-                      onClick={closeModal}
-                      className="ml-auto fill-current text-gray-700 w-6 h-6 cursor-pointer"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 18 18"
-                    >
-                      <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z" />
-                    </svg>
-                  </div>
-                  <hr />
-                  <div className="">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua.
-                  </div>
-                  <hr />
-                  <div className="ml-auto">
-                    <button
-                      className="bg-transparent hover:bg-gray-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-                      onClick={closeModal}
-                    >
-                      Close
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          )} */}
           <Transition appear show={modalOpen} as={Fragment}>
             <Dialog as="div" className="relative z-10" onClose={closeModal}>
               <Transition.Child
